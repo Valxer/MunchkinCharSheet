8f657e88e0c05c42c40d9048b6a818976a635e51
Kevin Le Van Phung <levanphungk@gmail.com>
2021-11-03 13:31:33 +0100

    added single char deletion
    can now delete a single char based on an  id passed on payload
    giving the id in char creation

--- Munchkin/src/store/chars.js
+++ Munchkin/src/store/chars.js
@@ -4,7 +4,22 @@ const state = {
 
 const mutations = {
     addChar(state, payload) {
+        let newId = 0
+        for (let i=0; i<state.chars.length; i++) {
+            newId =  state.chars[i].id + 1
+        }
+        payload.id = newId
+        console.log('payload : ', payload)
         state.chars.push(payload);
+    },
+    charDel(state, payload) {
+        let position
+        for (let i=0; i<state.chars.length; i++) {
+            if (state.chars[i].id == payload) {
+                position = i
+            }
+        }
+        state.chars.splice(position, 1)
     },
     clearState(state) {
         while (state.chars.length) {
@@ -17,6 +32,10 @@ const actions = {
     addChar({commit}, payload) {
         console.log('adding char')
         commit('addChar', payload)
+    },
+    charDel({commit}, payload) {
+        console.log('resetting char store')
+        commit('charDel', payload)
     },
     clearState({commit}) {
         console.log('resetting char store')
