31e829619bbca552f351b8b69ebfca29a899294f
Kevin Le Van Phung <levanphungk@gmail.com>
2021-11-05 13:43:48 +0100

    implemented essential datas and functions for fighting

--- Munchkin/src/pages/Fight.vue
+++ Munchkin/src/pages/Fight.vue
@@ -1,26 +1,51 @@
 <template>
-  <div class="fullscreen bg-primary text-secondary text-center q-pa-md flex flex-center">
+  <q-page class="fight flex flex-center page-container fight">
     <div>
-      <div style="font-size: 20vh">
-        Fight
-      </div>
-
+      <fightCard v-if="idFetched" :key="id" v-bind:char="char" />
       <q-btn
         class="q-mt-xl"
         color="secondary"
         text-color="primary"
         to="/"
-        label="Retour aux personnages"
+        label="Annuler le Combat"
         no-caps
       />
     </div>
-  </div>
+  </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+import fightCard from "../components/Fightcard.vue";
+import { mapState } from "vuex";
 
 export default defineComponent({
-  name: 'fight'
-})
+  name: "fight",
+  components: {
+    fightCard,
+  },
+  data() {
+    return {
+      id: "",
+      char: "",
+      lvlUp: [],
+      idFetched: false,
+    };
+  },
+  computed: {
+    ...mapState("chars", ["chars"]),
+  },
+  mounted() {
+    const url = window.location.pathname;
+    this.id = parseInt(url.substring(url.lastIndexOf("/") + 1), 10);
+    this.lvlUp.push(this.id);
+    for (let i = 0; i < this.chars.length; i++) {
+      if (this.chars[i].id == this.id) {
+        console.log(this.chars[i]);
+        this.char = this.chars[i];
+      }
+    }
+    this.idFetched = true;
+  },
+});
 </script>
