e9d832500d2d76cb41d025868be7681b1fc33201
Kevin Le Van Phung <levanphungk@gmail.com>
2021-11-05 11:23:30 +0100

    added sorting of cards

--- Munchkin/src/pages/Index.vue
+++ Munchkin/src/pages/Index.vue
@@ -6,207 +6,45 @@
       Ajoutez un joueur pour commencer
     </div>
 
-    <!-- Cards display section -->
+    <!-- Cards display section with level sorting-->
+    <div v-else-if="sort == true" class="cards-container">
+      <charCard
+        v-for="char in lvlSortedChars"    
+        :key="char.id"
+        v-bind:char= "char"/>
+    </div>
+
+    <!-- Cards display section with power sorting-->
+    <div v-else-if="sort == false" class="cards-container">
+      <charCard
+        v-for="char in powerSortedChars"    
+        :key="char.id"
+        v-bind:char= "char"/>
+    </div>
+
+    <!-- Cards display section without sorting-->
     <div v-else class="cards-container">
-      <!-- Ajouter les options de tri-->
-      <q-card
+      <charCard
         v-for="char in chars"    
         :key="char.id"
-        flat
-        bordered
-        class="my-card bg-accent">
-
-        <!-- Menu allowing to modify a character -->
-        <div class="menu-icon">
-          <q-btn color="primary" round flat icon="eva-edit-outline">
-            <q-menu cover auto-close>
-              <q-list>
-                <q-item clickable @click="deleteChar(char.id)">
-                  <q-item-section>Supprimmer personnage</q-item-section>
-                </q-item>
-                <q-item clickable @click="edRaces(char.id, char.race, char.race2)">
-                  <q-item-section>Modifier races</q-item-section>
-                </q-item>
-                <q-item clickable @click="edJobs(char.id, char.job, char.job2)">
-                  <q-item-section>Modifier classes</q-item-section>
-                </q-item>
-              </q-list>
-            </q-menu>
-          </q-btn>
-        </div>
-
-        <!-- Modal form to edit races-->
-        <q-dialog v-model="edRace" persistent class="dialog-container">
-          <q-card class="dialog-box">
-            <q-btn
-              class="close-btn"
-              v-close-popup
-              text-color="dark"
-              flat
-              icon-right="eva-close-outline"
-            />
-            <q-card-section class="column items-center q-mt-sm">
-              <q-avatar
-                icon="eva-edit-outline"
-                color="primary"
-                text-color="accent" />
-              <q-select
-                class="q-my-md formField"
-                standout="bg-accent text-secondary"
-                v-model="race"
-                :options="races"
-                label="Race" />
-              <q-select
-                class="q-mb-md formField"
-                standout="bg-accent text-secondary"
-                v-model="race2"
-                :options="races2"
-                label="Seconde race" />
-            </q-card-section>
-            <div v-if="edRaceError" class="error-msg">
-              Informations incorrectes :<br/>
-              Les deux races doivent être différentes<br/>
-            </div>
-            <q-card-actions align="center" class="q-mb-md">
-              <q-btn
-                flat
-                label="Confirmer"
-                color="primary"
-                @click="editRaces(id)" />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-
-        <!-- Modal form to edit jobs-->
-        <q-dialog v-model="edJob" persistent class="dialog-container">
-          <q-card class="dialog-box">
-            <q-btn
-              class="close-btn"
-              v-close-popup
-              text-color="dark"
-              flat
-              icon-right="eva-close-outline"
-            />
-            <q-card-section class="column items-center q-mt-sm">
-              <q-avatar
-                icon="eva-edit-outline"
-                color="primary"
-                text-color="accent" />
-              <q-select
-                class="q-my-md formField"
-                standout="bg-accent text-secondary"
-                v-model="job"
-                :options="jobs"
-                label="Race" />
-              <q-select
-                class="q-mb-md formField"
-                standout="bg-accent text-secondary"
-                v-model="job2"
-                :options="jobs"
-                label="Seconde race" />
-            </q-card-section>
-            <div v-if="edJobError" class="error-msg">
-              Informations incorrectes :<br/>
-              Les deux classes doivent être différentes<br/>
-              La deuxième classe ne peut pas exister sans première classe
-            </div>
-            <q-card-actions align="center" class="q-mb-md">
-              <q-btn
-                flat
-                label="Confirmer"
-                color="primary"
-                @click="editJobs(id)" />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-
-        <!-- Character description section -->
-        <q-card-section class="char-box">
-
-          <!-- name, races and classes of character -->
-          <div class="row items-center no-wrap">
-            <div class="col">
-              <div class="text-h4 name-box">
-                {{char.name}}
-                <q-icon class="text-h5 q-ml-sm" v-if="!char.sex" name="male"/>
-                <q-icon class="text-h5 q-ml-sm" v-else name="female"/>
-              </div>
-              <div class="text-subtitle2 q-mb-sm">
-                <div class="race-block">
-                  <span>
-                    {{char.race}}
-                  </span>
-                  <span v-if="char.race2 != 'Aucune'">
-                    /{{char.race2}}
-                  </span>
-                </div>
-                <div class="job-block q-mb-md">
-                  <span v-if="char.job != 'Aucune'">
-                    {{char.job}}
-                  </span>
-                  <span v-if="char.job2 != 'Aucune'">
-                    /{{char.job2}}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Level box -->
-          <div class="stat-section">
-            <div class="box1">
-              <div class="text-subtitle2">Niveau</div>
-              <q-btn-group class="stat-box bg-secondary">
-                <q-btn
-                  icon="eva-minus-outline"
-                  color="primary"
-                  @click="subLvl(char.id)" />
-                <span class="text-h6">
-                  {{char.lvl}}
-                </span>
-                <q-btn
-                  icon="eva-plus-outline"
-                  color="primary"
-                  @click="addLvl(char.id)" />
-              </q-btn-group>
-            </div>
-
-            <!-- Bonus box -->
-            <div>
-              <div class="text-subtitle2">Bonus</div>
-              <q-btn-group class="stat-box bg-secondary">
-                <q-btn
-                  icon="eva-minus-outline"
-                  color="primary"
-                  @click="subBonus(char.id)" />
-                <span class="text-h6">
-                  {{char.bonus}}
-                </span>
-                <q-btn
-                  icon="eva-plus-outline"
-                  color="primary"
-                  @click="addBonus(char.id)" />
-              </q-btn-group>
-            </div>
-          </div>
-
-          <!-- Adding lvl and bonuses to asses power -->
-          <div class="power-box text-h6">
-            PUISSANCE : {{char.power}}
-          </div>
-        </q-card-section>
-
-        <!-- Fight button -->
-        <q-card-actions>
-          <q-btn
-            @click="goToFight(char.id)"
-            class="fight-btn"
-            color="primary"
-            icon-right="mdi-sword-cross"
-            label="Combat" />
-        </q-card-actions>
-      </q-card>
+        v-bind:char= "char"/>
     </div>
+
+    <!-- Sort button -->
+    <q-page-sticky
+      v-if="chars.length"
+      position="top-right"
+      :offset="[18,18]" >
+      <div class="sort-sticky">
+        <div class="col">
+          Trier par : <strong>{{ sortOption }}</strong>
+        </div>
+        <q-toggle
+          toggle-indeterminate
+          v-model="sort"
+          @click="setSortOption()"/>
+      </div>
+    </q-page-sticky>
 
     <!-- Reset button -->
     <q-page-sticky
@@ -250,7 +88,6 @@
         </q-card>
       </q-dialog>
     </q-page-sticky>
-
 
     <!-- Add char button -->
     <q-page-sticky position="bottom-right" :offset="[18, 18]">
@@ -346,18 +183,20 @@
 import { defineComponent } from 'vue'
 import { ref } from 'vue'
 import { mapActions, mapState, mapGetters } from 'vuex'
+import charCard from '../components/CharCard.vue'
 
 export default defineComponent({
   name: 'home',
+  components: {
+    charCard
+  },
   data() {
     return {
       creationError: false,
       create: ref(false),
-      edJob: ref(false),
-      edJobError: false,
-      edRace: ref(false),
-      edRaceError: false,
       reset: ref(false),
+      sort: ref(null),
+      sortOption:'Ordre de création',
       //form storage
       sex: ref(false),
       id: '',
@@ -406,8 +245,15 @@ export default defineComponent({
     ...mapGetters('chars', ['lvlSortedChars', 'powerSortedChars'])
   },
   methods: {
-    ...mapActions('chars', ['addChar', 'clearState', 'charDel',
-        'racesEd', 'jobsEd', 'addLvl', 'subLvl', 'addBonus', 'subBonus']),
+    ...mapActions('chars', ['addChar', 'clearState']),
+    setSortOption() {
+      if (this.sort == true)
+        this.sortOption = 'Niveau'
+      else if (this.sort  == false)
+        this.sortOption = 'Puissance'
+      else
+        this.sortOption = "Ordre de création"
+    },
     resetValues() {
       this.id = ''
       this.name = ''
@@ -444,60 +290,9 @@ export default defineComponent({
         this.creationError = true
       }
     },
-    editRaces(id) {
-      if (this.race != this.race2) {
-        const updatedRaces = {}
-        updatedRaces.id = id
-        updatedRaces.race = this.race
-        updatedRaces.race2 = this.race2
-        this.racesEd(updatedRaces)
-        this.resetValues()
-        this.edRace = false
-      }
-      else {
-        this.edRaceError = true
-      }
-    },
-    editJobs(id) {
-      if ((this.job != this.job2) && this.job != 'Aucune' || this.job2 == 'Aucune') {
-        const updatedJobs = {}
-        updatedJobs.id = id
-        updatedJobs.job = this.job
-        updatedJobs.job2 = this.job2
-        this.jobsEd(updatedJobs)
-        this.resetValues()
-        this.edJob = false
-      }
-      else {
-        this.edJobError = true
-      }
-    },
-    deleteChar(id) {
-      this.charDel(id)
-    },
     resetState() {
       this.clearState()
       this.reset = false
-    },
-    edJobs(id, job, job2) {
-      this.id = id
-      this.job = job
-      this.job2 = job2
-      this.edJob = true
-    },
-    edRaces(id, race, race2) {
-      this.id = id
-      this.race = race
-      this.race2 = race2
-      this.edRace = true
-    },
-    goToFight(id) {
-      this.$router.push({
-        name: 'fight',
-        params: {
-          id: id
-        }
-      })
     }
   }
 })
@@ -516,57 +311,9 @@ export default defineComponent({
     align-items: center;
     flex-wrap: wrap;
   }
-  .my-card{
-    margin: 15px;
-    @media (max-width: $breakpoint-xs-max){
-        max-width: 365px;
-        min-width: 250px;
-    }
-  }
-  .menu-icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-    z-index: 1;
-    &:hover {
-      border-radius: 50px;
-      background-color: darken($color: $accent, $amount: 5);
-    }
-  }
-  .char-box {
-    padding-bottom: 5px;
-  }
-  .name-box {
+  .sort-sticky {
     display: flex;
-    justify-content: center;
     align-items: center;
-  }
-  .stat-section {
-    display: flex;
-    @media (max-width: $breakpoint-xs-max){
-        display: block;
-    }
-  }
-  .stat-box {
-    width: 168px;
-    span {
-      margin: auto auto;
-    }
-  }
-  .box1 {
-    @media (min-width: $breakpoint-xs-max) {
-      margin-right: 15px;
-    }
-    @media (max-width: $breakpoint-xs-max){
-        margin-bottom: 15px;
-    }
-  }
-  .power-box {
-    margin: 15px auto;
-    margin-bottom: 0px;
-  }
-  .fight-btn {
-    margin: 0 auto;
   }
   .create-btn {
     height: 45px;
