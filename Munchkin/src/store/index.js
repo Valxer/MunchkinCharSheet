708f26d6f29a298f14855bf26822121c5e17b2af
Kevin Le Van Phung <levanphungk@gmail.com>
2021-11-11 12:44:11 +0100

    adding monsters storage

--- Munchkin/src/store/index.js
+++ Munchkin/src/store/index.js
@@ -2,11 +2,13 @@ import { store } from 'quasar/wrappers'
 import { createStore } from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
 import chars from './chars'
+import monsters from './monsters'
 
 export default store(function (/* { ssrContext } */) {
   const Store = createStore({
     modules: {
-      chars
+      chars,
+      monsters
     },
     plugins:[createPersistedState()]
   })
---
+++ Munchkin/src/store/monsters.js
@@ -0,0 +1,20 @@
+const state = {
+    monsters: []
+}
+
+const getters = {
+}
+
+const mutations = {
+}
+
+const actions = {
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+}
